4d5fdc36c364e4ced5d7008e8bf98b26b61a1aa8
NavisWORLD <personal.phera@gmail.com>
2026-08-13 01:04:28 -0500

    Bump COSMOS Media version to 0.2.0

--- src/cosmos_media/__init__.py
+++ src/cosmos_media/__init__.py
@@ -5,4 +5,4 @@ from .state import CSTState, HebbianAssociator
 from .timeline import ChunkPlan, plan_timeline
 
 __all__ = ["Settings", "CSTState", "HebbianAssociator", "ChunkPlan", "plan_timeline"]
-__version__ = "0.1.0"
+__version__ = "0.2.0"
